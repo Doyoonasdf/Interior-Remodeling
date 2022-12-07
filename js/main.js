291977117612598bb71b5495925687057228fb70
Doyoonasdf <won1hana@naver.com>
2022-12-07 23:24:46 +0900

    12/7 슬라이더부분 완성

--- js/main.js
+++ js/main.js
@@ -1,47 +1,75 @@
-const services = document.querySelector("#services")
-const dts = services.querySelectorAll("dt");
-const dds = services.querySelectorAll("dd");
-
-const dts_icon = services.querySelectorAll("dt .icon");
-//tab키로 이동하게될 추적할 대상으로써 a태그를 사용했을뿐 a태그에는 큰의미는 없다
-
-dts_icon.forEach((el, index) => {
-  el.addEventListener("focusin", () => {
-    activation(dts, index);
-    activation(dds, index);
-  })
-})
 
 
+const Slider = document.querySelector(".slider")
+const ul = Slider.querySelector("ul");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const lis = ul.querySelectorAll("li");
+let len = lis.length; //추가가되도 자동 li의 갯수를 세어줌
+let enableClick = true;
+let speed = 500;
 
+//초기화 함수를 호출
+init();
 
-dts.forEach((el, index) => {
-  //배열에 반복을 돌면서 클릭이벤트 부여한다 
-  el.addEventListener("click", () => {
-    //모든 곳에 on을 떼었다가
-    // for (let el of dts) el.classList.remove("on");
-    // el.classList.add("on");
+//next 슬라이드 이동 코드
+next.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    // for(let el of dds){
-    //   el.classList.remove('on');
-    // } 
-    // dds[index].classList.add("on");
+  //재이벤트 방지구문(재클릭방지)
+  if (enableClick) {
 
-    //활성화 함수를 이름을 불러줌
-    activation(dts, index);
-    activation(dds, index);
-
-
-  })
-
-
-  //내가 선택한 그부분만 on을 붙임
-})
-
-//활성화 함수를 만듦
-function activation(arr, index) {
-  for (let el of arr) {
-    el.classList.remove("on");
+    nextSlide();
+    enableClick = false;
+    //함수 호출과 함께 false로 바꾸면서 모션진행시간을 보호해준다 그 시간동안은 버튼은 무력화된 상태
   }
-  arr[index].classList.add("on");
+
+
+})
+//prev 슬라이드 이동 코드
+prev.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (enableClick) {
+    prevSlide();
+    enableClick = false;
+  }
+})
+
+
+
+function init() {
+  ul.style.left = "-100%";
+  ul.prepend(ul.lastElementChild);
+  ul.style.width = `${100 * len}%`;
+  lis.forEach((el) => { el.style.width = `${100 / len}%`; })
+}
+//next 슬라이드 함수정의
+function nextSlide() {
+  new Anim(ul, {
+    prop: 'left',
+    value: "-200%",
+    duration: speed,
+    callback: () => {
+
+      ul.append(ul.firstElementChild); // 첫번째 li를  뒤쪽으로 보내는 코드
+      ul.style.left = "-100%"; //-200%에서 초기 위치 값으로 복귀하도록 하는 코드
+      enableClick = true;
+      //모션이 끝나고 다시 true바꾸면서 이벤트가 가능하도록한다
+
+    }
+  })
+}
+
+function prevSlide() {
+  new Anim(ul, {
+    prop: 'left',
+    value: "0%",
+    duration: speed,
+    callback: () => {
+      ul.style.left = "-100%"; //0%에서 초기 위치 값으로 복귀하는 코드
+      ul.prepend(ul.lastElementChild);//마지막 li를 맨앞으로 보내는 코드
+      enableClick = true;
+    }
+  })
 }
