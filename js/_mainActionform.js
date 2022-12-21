e656e1c67aa5fec2ef323f4f4958df221533ed67
Doyoonasdf <won1hana@naver.com>
2022-12-21 19:44:55 +0900

    12/21 메인페이지 액션폼 폰넘버 고침

--- js/_mainActionform.js
+++ js/_mainActionform.js
@@ -1,145 +1,127 @@
-const form = document.querySelector("#form");
-const btnSubmit = form.querySelector("input[type=submit]");
+const form = document.querySelector('#form');
+const btnSubmit = form.querySelector('input[type=submit]');
 
+btnSubmit.addEventListener('click', (e) => {
+	//이름
+	if (!isTxt('name')) e.preventDefault();
 
+	//이메일주소
+	if (!isEmail('email')) e.preventDefault();
 
+	//동의
+	if (!isAgree('agree')) e.preventDefault();
 
+	//핸드폰 (앞자리 중간자리 뒷자리)
 
-
-
-
-btnSubmit.addEventListener("click", (e) => {
-  //이름
-  if (!isTxt("name")) e.preventDefault();
-
-  //이메일주소
-  if (!isEmail("email")) e.preventDefault();
-
-  //동의
-  if (!isAgree("agree")) e.preventDefault();
-
-  //핸드폰 (앞자리 중간자리 뒷자리)
-
-  if (!isNum("tell")) e.preventDefault();
-  if (!isSelect("tell1")) e.preventDefault();
-
+	if (!isNum('tell')) e.preventDefault();
+	if (!isSelect('tell1')) e.preventDefault();
 });
 
-
-//name 이름은 빈값이면 안된다 빈값이면 안보낼거임 
+//name 이름은 빈값이면 안된다 빈값이면 안보낼거임
 
 function isTxt(el) {
-  let input = form.querySelector(`[name=${el}]`);
-  let txt = input.value;
+	let input = form.querySelector(`[name=${el}]`);
+	let txt = input.value;
 
-  // if (txt !== undefined) {  왜 언디파인드는 안될까
-  if (txt !== "") {
-    const errMsgs = input.closest("td").querySelectorAll("p");
+	// if (txt !== undefined) {  왜 언디파인드는 안될까
+	if (txt !== '') {
+		const errMsgs = input.closest('td').querySelectorAll('p');
 
-    if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
 
-    return true;
-  } else {
+		return true;
+	} else {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) return false;
 
-    const errMsgs = input.closest("td").querySelectorAll("p");
-    if (errMsgs.length > 0) return false;
-
-    const errMsg = document.createElement("p");
-    errMsg.append(`이름을 입력하세요`);
-    input.closest("td").append(errMsg);
-    return false;
-  }
+		const errMsg = document.createElement('p');
+		errMsg.append(`이름을 입력하세요`);
+		input.closest('td').append(errMsg);
+		return false;
+	}
 }
 
 //이메일은 @가 있어야한다
 function isEmail(el) {
-  let input = form.querySelector(`[name=${el}]`);
-  let txt = input.value;
+	let input = form.querySelector(`[name=${el}]`);
+	let txt = input.value;
 
+	if (/@/.test(txt)) {
+		const errMsgs = input.closest('td').querySelectorAll('p');
 
-  if (/@/.test(txt)) {
-    const errMsgs = input.closest("td").querySelectorAll("p");
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+		return true;
+	} else {
+		const errMsgs = input.closest('td').querySelectorAll('p');
 
-    if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
-    return true;
-  } else {
-    const errMsgs = input.closest("td").querySelectorAll("p");
-
-    if (errMsgs.length > 0) return false;
-    const errMsg = document.createElement("p");
-    errMsg.append("@를 포함한 전체 이메일 주소를 입력하세요");
-    input.closest("td").append(errMsg);
-    return false;
-  }
+		if (errMsgs.length > 0) return false;
+		const errMsg = document.createElement('p');
+		errMsg.append('@를 포함한 전체 이메일 주소를 입력하세요');
+		input.closest('td').append(errMsg);
+		return false;
+	}
 }
-
 
 //약관에 동의합니다에 체크해야한다
 function isAgree(el) {
-  let inputs = form.querySelectorAll(`[name=${el}]`);
-  let isCheck = false;
+	let inputs = form.querySelectorAll(`[name=${el}]`);
+	let isCheck = false;
 
-  for (let el of inputs) {
-    if (el.checked) isCheck = true;
-  }
+	for (let el of inputs) {
+		if (el.checked) isCheck = true;
+	}
 
-  if (isCheck) {
-    const errMsgs = inputs[0].closest("th").querySelectorAll("p");
+	if (isCheck) {
+		const errMsgs = inputs[0].closest('th').querySelectorAll('p');
 
-    if (errMsgs.length > 0) inputs[0].closest("th").querySelector("p").remove();
+		if (errMsgs.length > 0) inputs[0].closest('th').querySelector('p').remove();
 
+		return true;
+	} else {
+		const errMsgs = inputs[0].closest('th').querySelectorAll('p');
+		if (errMsgs.length > 0) return false;
 
-    return true;
-  } else {
-    const errMsgs = inputs[0].closest("th").querySelectorAll("p");
-    if (errMsgs.length > 0) return false;
+		const errMsg = document.createElement('p');
+		errMsg.append('필수 입력항목을 체크해주세요');
+		inputs[0].closest('th').append(errMsg);
 
-    const errMsg = document.createElement("p");
-    errMsg.append("필수 입력항목을 체크해주세요");
-    inputs[0].closest("th").append(errMsg);
-
-    return false;
-  }
-
-
+		return false;
+	}
 }
 
 //폰넘버는 8글자만 가능하다
 function isNum(el, len) {
-  if (len === undefined) len = 4;
-  let input = form.querySelector(`[name=${el}]`);
-  let txt = input.value;
-  const num = /[0-9]/;
+	if (len === undefined) len = 4;
+	let input = form.querySelector(`[name=${el}]`);
+	let txt = input.value;
+	const num = /^[0-9]+$/;
 
-  if (txt.length == len && num.test(txt)) {
-    const errMsgs = input.closest("td").querySelectorAll("p");
-    if (errMsgs.length > 0) input.closest("td").querySelector("p").remove();
-    return true;
-  } else {
-    const errMsgs = input.closest("td").querySelectorAll("p");
+	if (txt.length == len && num.test(txt)) {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+		return true;
+	} else {
+		const errMsgs = input.closest('td').querySelectorAll('p');
 
-    if (errMsgs.length > 0) return false;
+		if (errMsgs.length > 0) return false;
 
-    const errMsg = document.createElement("p");
-    errMsg.append(`핸드폰 번호를 입력해주세요`);
-    input.closest("td").append(errMsg);
-    return false;
-  }
-
+		const errMsg = document.createElement('p');
+		errMsg.append(`핸드폰 번호를 입력해주세요`);
+		input.closest('td').append(errMsg);
+		return false;
+	}
 }
 
-//셀렉트 010 부분 
+//셀렉트 010 부분
 function isSelect(el) {
-  let sel = form.querySelector(`[name=${el}]`);
-  let sel_index = sel.options.selectedIndex;
+	let sel = form.querySelector(`[name=${el}]`);
+	let sel_index = sel.options.selectedIndex;
 
-  let val = sel[sel_index].value;
+	let val = sel[sel_index].value;
 
-  if (val !== "") {
-
-    return true;
-  } else {
-    return false;
-  }
-
+	if (val !== '') {
+		return true;
+	} else {
+		return false;
+	}
 }
